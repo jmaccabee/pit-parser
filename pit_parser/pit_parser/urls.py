8ceced377913bcea7424e43f42f40c88922c5811
Josh Maccabee <josh.maccabee@gmail.com>
2023-04-29 00:02:24 -0400

    Add index view and template.

--- pit_parser/pit_parser/urls.py
+++ pit_parser/pit_parser/urls.py
@@ -17,6 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from .views import index
+
+urlpatterns = [path("admin/", admin.site.urls), path("", index, name="index")]
